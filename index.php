18de756089aa0afd9a3b5ca1638f2466e3025494
Karen Karoline Bastos <137943069+karenkaroline-bastos@users.noreply.github.com>
2024-11-23 16:29:58 +0000

    feat: incluindo o link para o nosso arquivo CSS

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <!-- Link para o Bootstrap, que é uma biblioteca de CSS e JS para estilização e funcionalidades -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Link para o nosso arquivo CSS externo (estilos.css) -->
-    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
